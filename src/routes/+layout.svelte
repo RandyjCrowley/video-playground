5063ae4642a8fa04fa79eb5ef7cb6ef429655368
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2024-04-13 19:22:54 +0800

    feat: menu

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav class="bg-slate-800 p-4">
+  <ul class="flex flex-wrap items-center">
+    <li>
+      <a
+        href="/"
+        class="text-white hover:text-teal-300 px-4 py-2 rounded-md text-lg font-medium"
+        >Home</a
+      >
+    </li>
+    <li>
+      <a
+        href="/videos"
+        class="text-white hover:text-teal-300 px-4 py-2 rounded-md text-lg font-medium"
+        >Videos</a
+      >
+    </li>
+  </ul>
+</nav>
+
+<div class="p-8">
+  <slot />
+</div>
