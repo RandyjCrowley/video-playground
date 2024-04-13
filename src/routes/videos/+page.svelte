423405781d6853ba1c9b068f82357a53c3817772
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2024-04-13 19:23:15 +0800

    style: videos page

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -2,13 +2,17 @@
   export let data;
 </script>
 
-<ul>
+<h2 class="mb-4 text-2xl font-bold text-white">Videos</h2>
+<ul class="list-none m-0 p-0">
   {#each data.videos as video}
-    <a href={`videos/${video.slug}`}>
-      <li>
-        <h3>{video.title}</h3>
-        <p>{video.description}</p>
-      </li>
-    </a>
+    <li class="mb-4">
+      <a
+        href={`videos/${video.slug}`}
+        class="flex flex-col no-underline hover:text-teal-300"
+      >
+        <h3 class="text-xl text-white font-bold">{video.title}</h3>
+        <p class="text-gray-300">{video.description}</p>
+      </a>
+    </li>
   {/each}
 </ul>
