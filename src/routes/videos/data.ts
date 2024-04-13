8abd5efca583017fa50d79741108a650e4610ce7
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2024-04-13 12:36:43 +0800

    chore: add new video

--- src/routes/videos/data.ts
+++ src/routes/videos/data.ts
@@ -5,4 +5,10 @@ export const videos: Video[] = [
     description:
       'Find out what Jung thought of astrology and how he used it in his practice.'
   },
+  {
+    slug: 'how-to-become-articulate',
+    title: 'How to Become More Articulate?',
+    description:
+      'Discover Jordan Peterson\'s secrets to gaining mastery over your mind and communicating effectively with the world.'
+  },
 ];
