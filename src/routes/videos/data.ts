633b17cc0bbea75e291578cdcd27d11976d5db92
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2024-04-15 11:57:46 +0800

    chore: add new video

--- src/routes/videos/data.ts
+++ src/routes/videos/data.ts
@@ -1,14 +1,14 @@
 export const videos: Video[] = [
   {
-    slug: 'jung-astrology',
-    title: 'What did Jung say about astrology?',
-    description:
-      'Find out what Jung thought of astrology and how he used it in his practice.'
-  },
-  {
     slug: 'how-to-become-articulate',
     title: 'How to Become More Articulate?',
     description:
       'Discover Jordan Peterson\'s secrets to gaining mastery over your mind and communicating effectively with the world.'
+  },
+  {
+    slug: 'why-not-afraid-of-dying',
+    title: 'Why You Shouldn\'t Be Afraid of Dying',
+    description:
+      'Hear Dr. Peterson\'s advice on how to be at peace with death.'
   },
 ];
